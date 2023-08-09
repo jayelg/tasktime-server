d28d9b6d84a6831e6de76f02b7a331e350a93e64
John Grant <john@raur.net>
2023-08-10 00:33:42 +1000

    add event emitter for sendMagicLink

--- src/auth/magicLogin.strategy.ts
+++ src/auth/magicLogin.strategy.ts
@@ -3,17 +3,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import Strategy from 'passport-magic-login';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
-import { MailService } from 'src/mail/mail.service';
-import { UserService } from 'src/user/user.service';
-// import { createTransport } from 'nodemailer';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { MagicLoginEvent } from './event/magicLogin.event';
 
 @Injectable()
 export class MagicLoginStrategy extends PassportStrategy(Strategy) {
   constructor(
     private readonly configService: ConfigService,
     private authService: AuthService,
-    private mailService: MailService,
-    private userService: UserService,
+    private eventEmitter: EventEmitter2,
   ) {
     super({
       secret: configService.get<string>('JWT_SECRET_KEY'),
@@ -24,8 +22,11 @@ export class MagicLoginStrategy extends PassportStrategy(Strategy) {
         'SERVER_URL',
       )}:${configService.get<string>('PORT')}/auth/login/callback`,
       sendMagicLink: async (destination, href) => {
-        console.log(`got email ${destination} and href ${href}`);
-        await this.mailService.sendMagicLink(destination, href);
+        // listener at user.Service
+        this.eventEmitter.emit(
+          'magicLogin.login',
+          new MagicLoginEvent(destination, href),
+        );
       },
       verify: async (payload, callback) => {
         callback(null, this.validate(payload));
