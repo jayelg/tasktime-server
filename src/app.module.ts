60a3db2a18292fa745f272d7152868b62da44144
John Grant <john@raur.net>
2023-08-09 14:03:22 +1000

    Added Nest DevTools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,28 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { OrgModule } from './org/org.module';
-// import { UserService } from './user/user.service';
+
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { EventModule } from './event/event.module';
 import { JwtMiddleware } from './auth/jwt.middleware';
 import { JwtService } from '@nestjs/jwt';
-import { MailModule } from './mail/mail.module';
 import { accessibleRecordsPlugin } from '@casl/mongoose';
+// Modules
+import { UserModule } from './user/user.module';
+import { OrgModule } from './org/org.module';
+import { ProjectModule } from './project/project.module';
+import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notification/notification.module';
 import { MessageModule } from './message/message.module';
+import { ItemModule } from './item/item.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -31,9 +37,14 @@ import { MessageModule } from './message/message.module';
       }),
       inject: [ConfigService],
     }),
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
     AuthModule,
     UserModule,
     OrgModule,
+    ProjectModule,
+    ItemModule,
     EventModule,
     MailModule,
     NotificationModule,
@@ -49,6 +60,6 @@ import { MessageModule } from './message/message.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(JwtMiddleware).forRoutes('*'); // Apply the middleware to all routes
+    consumer.apply(JwtMiddleware).forRoutes('*'); // Apply the jwt decrypting middleware to all routes
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { snapshot: true });
   const configService = app.get(ConfigService);
 
   const appName = configService.get<string>('APP_NAME');
