3ad4f05bd8a88ed9f1067dc4c9c1434f64c1a6ce
John Grant <john@raur.net>
2023-08-09 14:02:29 +1000

    added comment

--- src/user/interface/user.interface.ts
+++ src/user/interface/user.interface.ts
@@ -4,7 +4,7 @@ export interface IUser {
   firstName: string;
   lastName: string;
   avatar: string;
-  orgs: string[];
+  orgs: string[]; // stored for better db query. the single point of truth is org.members
   personalProjects: string[];
   createdAt: string;
   lastLoginAt: string;
