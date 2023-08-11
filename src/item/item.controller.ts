57be4bbcc481e21619a84a3c38731a37dc7ee0d2
John Grant <john@raur.net>
2023-08-11 18:09:00 +1000

    added checkAbilities

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -13,85 +13,88 @@ import { Item } from './item.schema';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { NewItemDto } from './dto/newItem.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  CheckAbilities,
+  CreateItemAbility,
+  DeleteItemAbility,
+  ManageProjectAbility,
+  UpdateItemAbility,
+  ViewProjectAbility,
+} from 'src/ability/abilities.decorator';
+import { IItem } from './interface/item.interface';
+import { UpdateItemDto } from './dto/updateItem.dto';
 
 @Controller('org/:orgId/projects/:projectId/items')
 @ApiTags('items')
 export class ItemController {
   constructor(private itemService: ItemService) {}
 
-  // replace with class-validator dto
-  private checkRequest(
-    requiredProps: string[],
-    request: any,
-    optionalProps: string[] = [],
-  ) {
-    const missingProps = [];
-    const invalidProps = [];
-    const allowedProps = [...requiredProps, ...optionalProps];
-    for (const property of requiredProps) {
-      if (!request.hasOwnProperty(property) || !request[property]) {
-        missingProps.push(property);
-      }
-    }
-    if (allowedProps.length > 0) {
-      for (const property in request) {
-        if (!allowedProps.includes(property)) {
-          invalidProps.push(property);
-        }
-      }
-    }
-    if (missingProps.length > 0 || invalidProps.length > 0) {
-      throw new Error(
-        (missingProps.length > 0
-          ? 'The following properties are missing: ' +
-            missingProps.join(', ') +
-            '. '
-          : '') +
-          (invalidProps.length > 0
-            ? 'The following properties are either not valid or cannot be modified: ' +
-              invalidProps.join(', ') +
-              '.'
-            : ''),
-      );
-    }
-  }
+  // replaced with class-validator dto
+  // private checkRequest(
+  //   requiredProps: string[],
+  //   request: any,
+  //   optionalProps: string[] = [],
+  // ) {
+  //   const missingProps = [];
+  //   const invalidProps = [];
+  //   const allowedProps = [...requiredProps, ...optionalProps];
+  //   for (const property of requiredProps) {
+  //     if (!request.hasOwnProperty(property) || !request[property]) {
+  //       missingProps.push(property);
+  //     }
+  //   }
+  //   if (allowedProps.length > 0) {
+  //     for (const property in request) {
+  //       if (!allowedProps.includes(property)) {
+  //         invalidProps.push(property);
+  //       }
+  //     }
+  //   }
+  //   if (missingProps.length > 0 || invalidProps.length > 0) {
+  //     throw new Error(
+  //       (missingProps.length > 0
+  //         ? 'The following properties are missing: ' +
+  //           missingProps.join(', ') +
+  //           '. '
+  //         : '') +
+  //         (invalidProps.length > 0
+  //           ? 'The following properties are either not valid or cannot be modified: ' +
+  //             invalidProps.join(', ') +
+  //             '.'
+  //           : ''),
+  //     );
+  //   }
+  // }
 
   @Get()
+  @CheckAbilities(new ViewProjectAbility())
   async getAllItems(@Param('projectId') projectId: string) {
     return this.itemService.getAllItems(projectId);
   }
 
   @Get(':itemId')
-  async getItems(
-    @Param('projectId') projectId: string,
-    @Param('itemId') itemId: string,
-  ): Promise<Item> {
-    const requiredProps = ['projectId', 'itemId'];
-    this.checkRequest(requiredProps, { projectId: projectId, itemId: itemId });
-    return this.itemService.getItem(projectId, itemId);
+  @CheckAbilities(new ViewProjectAbility())
+  async getItems(@Param('itemId') itemId: string): Promise<IItem> {
+    return this.itemService.getItem(itemId);
   }
 
   @Post()
+  @CheckAbilities(new CreateItemAbility())
   async createItem(
     @Req() req,
     @Param('projectId') projectId: string,
     @Body() newItem: NewItemDto,
   ) {
-    const requiredProps = ['name', 'creator', 'colour'];
-    const optionalProps = ['parentItemId', 'predecessorItemId'];
-    this.checkRequest(requiredProps, newItem, optionalProps);
     const createdItem = await this.itemService.createItem(
       req.user._id,
       projectId,
       newItem,
     );
 
+    // move to service
     // update parent nested item relationships
     if (createdItem.parentItemId.toString() !== projectId) {
-      const parentItem = await this.itemService.getItem(
-        projectId,
-        newItem.parentItemId,
-      );
+      const parentItem = await this.itemService.getItem(newItem.parentItemId);
       const updatedNestedItemIds = {
         nestedItemIds: [
           ...parentItem.nestedItemIds,
@@ -112,15 +115,16 @@ export class ItemController {
   }
 
   @Patch(':itemId')
+  @CheckAbilities(new UpdateItemAbility())
   async updateItem(
-    @Param('projectId') projectId: string,
     @Param('itemId') itemId: string,
-    @Body() changes: object,
+    @Body() changes: UpdateItemDto,
   ) {
     return await this.itemService.updateItem(itemId, changes);
   }
 
   @Delete(':itemId')
+  @CheckAbilities(new DeleteItemAbility())
   async deleteItem(
     @Param('projectId') projectId: string,
     @Param('itemId') itemId: string,
@@ -129,6 +133,7 @@ export class ItemController {
   }
 
   @Delete()
+  @CheckAbilities(new ManageProjectAbility())
   async deleteAllItems(@Param('projectId') projectId: string) {
     return await this.itemService.deleteAllItems(projectId);
   }
