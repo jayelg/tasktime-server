befb4d979c51286abb845fed75136855e76c1aac
John Grant <john@raur.net>
2023-08-11 16:42:44 +1000

    extend request Type with props added by passport

---
+++ src/ability/customRequest.interface.ts
@@ -0,0 +1,12 @@
+declare module 'express' {
+  export interface Request {
+    user: {
+      id: string;
+      email: string;
+    };
+    params: {
+      orgId: string;
+      projectId: string;
+    };
+  }
+}
