b797edca09150ea25aa0f76ab39f42fb65c1e555
John Grant <john@raur.net>
2023-08-11 16:41:46 +1000

    added ability functions

--- src/ability/abilities.decorator.ts
+++ src/ability/abilities.decorator.ts
@@ -1,5 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 import { Action, Subjects } from './ability.factory';
+import { Org } from 'src/org/org.schema';
+import { Project } from 'src/project/project.schema';
 
 export interface RequiredRule {
   action: Action;
@@ -8,5 +10,59 @@ export interface RequiredRule {
 
 export const CHECK_ABILITY = 'check_ability';
 
-export const CheckAbilitiesDecorator = (...requirements: RequiredRule[]) =>
+export const CheckAbilities = (...requirements: RequiredRule[]) =>
   SetMetadata(CHECK_ABILITY, requirements);
+
+// todo more
+
+// Org
+export class ManageOrgAbility implements RequiredRule {
+  action = Action.Manage;
+  subject = Org;
+}
+
+export class CreateOrgAbility implements RequiredRule {
+  action = Action.Create;
+  subject = Org;
+}
+
+export class ViewOrgAbility implements RequiredRule {
+  action = Action.Read;
+  subject = Org;
+}
+
+export class UpdateOrgAbility implements RequiredRule {
+  action = Action.Update;
+  subject = Org;
+}
+
+export class DeleteOrgAbility implements RequiredRule {
+  action = Action.Delete;
+  subject = Org;
+}
+
+// Project
+export class ManageProjectAbility implements RequiredRule {
+  action = Action.Manage;
+  subject = Project;
+}
+
+export class CreateProjectAbility implements RequiredRule {
+  action = Action.Create;
+  subject = Project;
+}
+
+export class ViewProjectAbility implements RequiredRule {
+  action = Action.Read;
+  subject = Project;
+}
+
+export class UpdateProjectAbility implements RequiredRule {
+  action = Action.Update;
+  subject = Project;
+}
+
+export class DeleteProjectAbility implements RequiredRule {
+  action = Action.Delete;
+  subject = Project;
+}
