c0aa7f52990be035d68777030f67833f04d9e9af
John Grant <john@raur.net>
2023-08-16 11:08:52 +1000

    simplified constructor

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { UserDocument } from '../user.schema';
+import { User } from '../user.schema';
 
 // shape of user.schema.ts
 // represents object returned from db
@@ -17,52 +17,22 @@ export class UserDto {
   unreadMessages: string[];
   disabled: boolean;
 
-  constructor(data: UserDocument | UserDto) {
-    if (data instanceof mongoose.Document) {
-      const user = data.toJSON();
-      this._id = data._id.toString();
-      this.email = user.email;
-      this.firstName = user.firstName;
-      this.lastName = user.lastName;
-      this.avatar = user.avatar;
-      this.orgs = user.orgs.map((org) => org._id.toString());
-      this.personalProjects = user.personalProjects.map((project) =>
-        project._id.toString(),
-      );
-      this.unreadNotifications = user.unreadNotifications.map((note) =>
-        note._id.toString(),
-      );
-      this.unreadMessages = user.unreadMessages.map((message) =>
-        message._id.toString(),
-      );
-      this.disabled = user.disabled;
-    } else if (
-      'email' in data &&
-      'firstName' in data &&
-      'lastName' in data &&
-      'avatar' in data &&
-      'orgs' in data &&
-      'personalProjects' in data &&
-      'createdAt' in data &&
-      'lastLoginAt' in data &&
-      'unreadNotifications' in data &&
-      'unreadMessages' in data &&
-      'disabled' in data
-    ) {
-      this._id = data._id;
-      this.email = data.email;
-      this.firstName = data.firstName;
-      this.lastName = data.lastName;
-      this.avatar = data.avatar;
-      this.orgs = data.orgs;
-      this.personalProjects = data.personalProjects;
-      this.createdAt = data.createdAt;
-      this.lastLoginAt = data.lastLoginAt;
-      this.unreadNotifications = data.unreadNotifications;
-      this.unreadMessages = data.unreadMessages;
-      this.disabled = data.disabled;
-    } else {
-      throw new Error('Invalid arguments for UserDto constructor.');
-    }
+  constructor(data: User | UserDto) {
+    this._id = data._id.toString();
+    this.email = data.email;
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.avatar = data.avatar;
+    this.orgs = data.orgs.map((org) => org._id.toString());
+    this.personalProjects = data.personalProjects.map((project) =>
+      project._id.toString(),
+    );
+    this.unreadNotifications = data.unreadNotifications.map((note) =>
+      note._id.toString(),
+    );
+    this.unreadMessages = data.unreadMessages.map((message) =>
+      message._id.toString(),
+    );
+    this.disabled = data.disabled;
   }
 }
