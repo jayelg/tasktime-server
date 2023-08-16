e3491be7e3f775de9233379f288f18aee6b7e0ef
John Grant <john@raur.net>
2023-08-16 15:40:10 +1000

    removed unused import

--- src/org/org.module.ts
+++ src/org/org.module.ts
@@ -3,7 +3,6 @@ import { OrgController } from './org.controller';
 import { OrgService } from './org.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrgSchema } from './org.schema';
-import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Org', schema: OrgSchema }])],
