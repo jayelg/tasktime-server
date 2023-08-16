12683586f076a4dc2ec80c217f185c375d2db22a
John Grant <john@raur.net>
2023-08-16 19:45:22 +1000

    setup mock dependancies

--- src/mail/test/mail.service.spec.ts
+++ src/mail/test/mail.service.spec.ts
@@ -1,44 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from '../mail.service';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { UserModule } from 'src/user/user.module';
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 
 describe('MailService', () => {
   let service: MailService;
 
+  const mockMailerService = {
+    sendMail: jest.fn(),
+  };
+
+  const mockConfigService = {
+    get: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot(),
-        UserModule
-        MailerModule.forRootAsync({
-          imports: [ConfigModule], // Import ConfigModule here too
-          useFactory: async (configService: ConfigService) => ({
-            transport: {
-              host: configService.get<string>('SMTP_URL'),
-              secure: false,
-              auth: {
-                user: configService.get<string>('SMTP_USER'),
-                pass: configService.get<string>('SMTP_PASS'),
-              },
-            },
-            defaults: {
-              from: `"${configService.get<string>(
-                'APP_NAME',
-              )}" <${configService.get<string>('SMTP_FROM_ADDRESS')}>`,
-            },
-            template: {
-              dir: join(__dirname, 'templates'),
-              adapter: new HandlebarsAdapter(),
-              options: {
-                strict: true,
-              },
-            },
-          }),
-          inject: [ConfigService], // Inject ConfigService here
-        }),
+      providers: [
+        MailService,
+        {
+          provide: MailerService,
+          useValue: mockMailerService,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
       ],
-      providers: [MailService],
     }).compile();
 
     service = module.get<MailService>(MailService);
