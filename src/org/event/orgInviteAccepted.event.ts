87cd6f400799920bfe5cf3a2ae5439a706d46678
John Grant <john@raur.net>
2023-08-17 09:36:15 +1000

    created event

---
+++ src/org/event/orgInviteAccepted.event.ts
@@ -0,0 +1,7 @@
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+export class OrgInviteAcceptedEvent extends EventEmitter2 {
+  constructor(public readonly userId: string, public readonly orgId: string) {
+    super();
+  }
+}
