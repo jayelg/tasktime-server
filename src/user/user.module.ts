a97ee0343348f61c26bd4f9488b948ae000d6dec
John Grant <john@raur.net>
2023-08-17 11:35:25 +1000

    added UserListenersService provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './user.schema';
 import { UserController } from './user.controller';
+import { UserListenersService } from './listeners/userListeners.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserListenersService],
   exports: [UserService],
 })
 export class UserModule {}
