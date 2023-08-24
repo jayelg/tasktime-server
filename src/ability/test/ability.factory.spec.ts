67dc17a6878bb50795d1eee1a3ff3d490e016d87
John Grant <john@raur.net>
2023-08-24 11:57:31 +1000

    added mock dependancies

--- src/ability/test/ability.factory.spec.ts
+++ src/ability/test/ability.factory.spec.ts
@@ -1,7 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { AbilityFactory } from '../ability.factory';
+import { OrgService } from 'src/org/org.service';
+import { ProjectService } from 'src/project/project.service';
+import { ItemService } from 'src/item/item.service';
 
 describe('AbilityFactory', () => {
+  let service: AbilityFactory;
+
+  const mockOrgService = {
+    getItem: jest.fn(),
+  };
+
+  const mockProjectService = {
+    getItem: jest.fn(),
+  };
+
+  const mockItemService = {
+    getItem: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AbilityFactory, OrgService, ProjectService, ItemService],
+    })
+      .overrideProvider(OrgService)
+      .useValue(mockOrgService)
+      .overrideProvider(ProjectService)
+      .useValue(mockProjectService)
+      .overrideProvider(ItemService)
+      .useValue(mockItemService)
+      .compile();
+
+    service = module.get<AbilityFactory>(AbilityFactory);
+  });
+
   it('should be defined', () => {
-    expect(new AbilityFactory()).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
