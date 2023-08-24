e7bf152114b532c80933bcfc9062b58e03de6c6e
John Grant <john@raur.net>
2023-08-24 12:00:50 +1000

    fixed error by defining orgs type and optionality

--- src/user/dto/updateUserRequest.dto.ts
+++ src/user/dto/updateUserRequest.dto.ts
@@ -1,14 +1,7 @@
-import {
-  IsString,
-  IsNotEmpty,
-  IsUrl,
-  IsOptional,
-  IsBoolean,
-} from 'class-validator';
 import { ArrayModification, UpdateUserDto } from './updateUser.dto';
 import { Exclude } from 'class-transformer';
 
 export class UpdateUserRequestDto extends UpdateUserDto {
   @Exclude()
-  orgs; // set from the org module and updated as an event effect
+  orgs?: ArrayModification;
 }
