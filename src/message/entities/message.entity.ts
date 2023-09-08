5e638463e3d599fa947f82759a1c12aca32cfd45
John Grant <john@raur.net>
2023-09-09 00:08:21 +1000

    initial commit

---
+++ src/message/entities/message.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  EntityRepositoryType,
+  ManyToOne,
+  Property,
+  Reference,
+} from '@mikro-orm/core';
+import { CustomBaseEntity } from 'src/shared/entities/customBase.entity';
+import { User } from 'src/user/entities/user.entity';
+import { MessageRepository } from '../repositories/message.repository';
+
+@Entity({ customRepository: () => MessageRepository })
+export class Message extends CustomBaseEntity {
+  [EntityRepositoryType]?: MessageRepository;
+
+  @ManyToOne(() => User)
+  sender: Reference<User>;
+
+  @ManyToOne(() => User)
+  recipient: Reference<User>;
+
+  @Property()
+  read = false;
+
+  @Property()
+  title: string;
+
+  @Property()
+  body: string;
+}
