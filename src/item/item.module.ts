bcfa09b80c938a4cfe39b10fd6c33c39c8983986
John Grant <john@raur.net>
2023-09-08 21:18:44 +1000

    added mikro-orm imports, added ItemRepository provider, removed mongoose import,

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { ItemController } from './item.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ItemSchema } from './item.schema';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Item } from './entities/item.entity';
+import { ItemMember } from './entities/itemMember.entity';
+import { ItemRepository } from './repositories/item.repository';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Item', schema: ItemSchema }])],
-  providers: [ItemService],
+  imports: [MikroOrmModule.forFeature([Item, ItemMember])],
+  providers: [ItemService, ItemRepository],
   controllers: [ItemController],
   exports: [ItemService],
 })
