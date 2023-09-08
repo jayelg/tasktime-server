33bc9ca29cbd43c32c2eaf93368d3260c760842b
John Grant <john@raur.net>
2023-09-08 21:31:18 +1000

    update to simplify types

--- src/notification/event/notificationCreated.event.ts
+++ src/notification/event/notificationCreated.event.ts
@@ -1,8 +1,8 @@
 import { EventEmitter2 } from '@nestjs/event-emitter';
-import { INotification } from '../interface/notification.interface';
+import { Notification } from '../entities/notification.entity';
 
 export class NotificationCreatedEvent extends EventEmitter2 {
-  constructor(public readonly notification: INotification) {
+  constructor(public readonly notification: Notification) {
     super();
   }
 }
