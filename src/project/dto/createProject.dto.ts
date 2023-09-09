20b306b189002e8b630793a3fbacfaae2b754788
John Grant <john@raur.net>
2023-09-10 09:12:38 +1000

    removed orgId as this now in url params

--- src/project/dto/createProject.dto.ts
+++ src/project/dto/createProject.dto.ts
@@ -1,11 +1,7 @@
-import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateProjectDto {
   @IsString()
   @IsNotEmpty()
   name: string;
-
-  @IsNumber()
-  @IsNotEmpty()
-  orgId: number;
 }
