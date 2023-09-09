4dcce7b2f49326a860af34b5c9e422b5254e510f
John Grant <john@raur.net>
2023-09-10 09:14:58 +1000

    removed broken listeners aggregator service. imported listeners directly.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { UserListenersService } from './listeners/userListeners.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UserRepository } from './repositories/user.repository';
 import { User } from './entities/user.entity';
+import AuthValidateUserListener from './listeners/authValidateUser.listener';
 
 @Module({
   imports: [MikroOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, UserRepository, UserListenersService],
+  providers: [UserService, UserRepository, AuthValidateUserListener],
   exports: [UserService],
 })
 export class UserModule {}
