8b8c0827ca6ec2498727d8f3446f64c28428a105
John Grant <john@raur.net>
2023-09-10 09:06:35 +1000

    changed id types for uuid fix findOrgMember logic

--- src/org/repositories/org.repository.ts
+++ src/org/repositories/org.repository.ts
@@ -3,18 +3,14 @@ import { Org } from '../entities/org.entity';
 import { OrgMember } from '../entities/orgMember.entity';
 
 export class OrgRepository extends EntityRepository<Org> {
-  async findMembersByOrgId(orgId: number): Promise<OrgMember[]> {
+  async findMembersByOrgId(orgId: string): Promise<OrgMember[]> {
     return this.em.find(OrgMember, { org: orgId });
   }
 
-  async findOrgMember(userId: number, orgId: number): Promise<OrgMember> {
-    const orgMember = await this.em.findOne(OrgMember, {
+  async findOrgMember(userId: string, orgId: string): Promise<OrgMember> {
+    return await this.em.findOne(OrgMember, {
       member: userId,
       org: orgId,
     });
-    if (orgMember) {
-      await orgMember.member.load();
-    }
-    return orgMember;
   }
 }
