218199df9c9ef4b443982ef5c762b512db02065c
John Grant <john@raur.net>
2023-09-10 08:58:35 +1000

    added find member by id method

--- src/project/repositories/project.repository.ts
+++ src/project/repositories/project.repository.ts
@@ -3,7 +3,17 @@ import { Project } from '../entities/project.entity';
 import { ProjectMember } from '../entities/projectMember.entity';
 
 export class ProjectRepository extends EntityRepository<Project> {
-  async findMembersByProjectId(projectId: number): Promise<ProjectMember[]> {
+  async findMembersByProjectId(projectId: string): Promise<ProjectMember[]> {
     return this.em.find(ProjectMember, { project: projectId });
+  }
+
+  async findProjectMember(
+    userId: string,
+    projectId: string,
+  ): Promise<ProjectMember> {
+    return await this.em.findOne(ProjectMember, {
+      member: userId,
+      project: projectId,
+    });
   }
 }
