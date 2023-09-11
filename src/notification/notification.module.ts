c81ce01acdb0aeb710bfd2f9d23a508f0dc1edf3
John Grant <john@raur.net>
2023-09-11 11:54:40 +1000

    adde repository to providers

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -3,10 +3,11 @@ import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Notification } from './entities/notification.entity';
+import { NotificationRepository } from './repositories/notification.repository';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Notification])],
-  providers: [NotificationService],
+  providers: [NotificationService, NotificationRepository],
   exports: [NotificationService],
   controllers: [NotificationController],
 })
