229bbb0798ee9fc72277e5d8a0a0f783f5e9dbd2
John Grant <john@raur.net>
2023-10-03 11:05:35 +1100

    added UserLogin Listener to providers

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { ConfigModule } from '@nestjs/config';
 import { MailerService } from './mailer.service';
+import UserLoginListener from './listeners/userLogin.event';
 
 @Global()
 @Module({
@@ -10,7 +11,7 @@ import { MailerService } from './mailer.service';
       isGlobal: true,
     }),
   ],
-  providers: [MailService, MailerService],
+  providers: [MailService, MailerService, UserLoginListener],
   exports: [MailService],
 })
 export class MailModule {}
