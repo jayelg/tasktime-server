8465a3734b02ce195c3e4f79f178be90886442a9
John Grant <john@raur.net>
2023-10-18 13:39:58 +1100

    updated imports, restructured to /api/*

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,17 +1,17 @@
 import { Options } from '@mikro-orm/core';
-import { ConfigService } from '@nestjs/config';
-import { Item } from './item/entities/item.entity';
-import { ItemMember } from './item/entities/itemMember.entity';
-import { User } from './user/entities/user.entity';
-import { Org } from './org/entities/org.entity';
-import { OrgMember } from './org/entities/orgMember.entity';
-import { Project } from './project/entities/project.entity';
-import { ProjectMember } from './project/entities/projectMember.entity';
+// import { ConfigService } from '@nestjs/config';
+import { Item } from './api/item/entities/item.entity';
+import { ItemMember } from './api/item/entities/itemMember.entity';
+import { User } from './api/user/entities/user.entity';
+import { Org } from './api/org/entities/org.entity';
+import { OrgMember } from './api/org/entities/orgMember.entity';
+import { Project } from './api/project/entities/project.entity';
+import { ProjectMember } from './api/project/entities/projectMember.entity';
 import { CustomBaseEntity } from './shared/entities/customBase.entity';
-import { Notification } from './notification/entities/notification.entity';
-import { ItemAncestry } from './item/entities/itemAncestry.entity';
+import { Notification } from './api/notification/entities/notification.entity';
+import { ItemAncestry } from './api/item/entities/itemAncestry.entity';
 
-export default (configService: ConfigService): Options => {
+export default (/* configService: ConfigService */): Options => {
   return {
     // debug: true,
     entities: [
