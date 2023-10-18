b0cdfa027197256cfe64442ec573906f03c84088
John Grant <john@raur.net>
2023-10-18 16:12:57 +1100

    merged seperate ui controllers into app-ui

--- src/app-ui/protected-ui/protected-ui.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { join } from 'path';
-
-@Injectable()
-export class ProtectedUiService {
-  getProtectedAppPath(): string {
-    return join('static', 'protected');
-  }
-}
--- src/app-ui/public-ui/public-ui.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { join } from 'path';
-
-@Injectable()
-export class PublicUiService {
-  getPublicAppPath(): string {
-    return join('static', 'public');
-  }
-}
