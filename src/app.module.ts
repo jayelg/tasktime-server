d1851bee436acbc34cc4966a0237447f03ccc552
John Grant <john@raur.net>
2023-10-18 13:36:34 +1100

    migrate child modules of api to the api module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,47 +1,31 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { EventModule } from './event/event.module';
+import { JwtAuthGuard } from './shared/guards/jwt-auth.guard';
 import { JwtService } from '@nestjs/jwt';
-// Modules
-import { UserModule } from './user/user.module';
-import { OrgModule } from './org/org.module';
-import { ProjectModule } from './project/project.module';
-import { MailModule } from './mail/mail.module';
-import { AuthModule } from './auth/auth.module';
-import { NotificationModule } from './notification/notification.module';
-import { MessageModule } from './message/message.module';
-import { ItemModule } from './item/item.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { AbilityModule } from './ability/ability.module';
-import { AbilitiesGuard } from './ability/abilities.guard';
+import { AbilityModule } from './api/ability/ability.module';
+import { AbilitiesGuard } from './shared/guards/abilities.guard';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import mikroOrmConfig from './mikro-orm.config';
+import { ApiModule } from './api/api.module';
+import { AppUiModule } from './app-ui/app-ui.module';
 
 @Module({
   imports: [
+    ApiModule,
+    AppUiModule,
+    AbilityModule,
     EventEmitterModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     MikroOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) =>
-        mikroOrmConfig(configService),
+      useFactory: (/* configService: ConfigService */) =>
+        mikroOrmConfig(/* configService */),
       inject: [ConfigService],
     }),
-    AuthModule,
-    UserModule,
-    OrgModule,
-    ProjectModule,
-    ItemModule,
-    EventModule,
-    MailModule,
-    NotificationModule,
-    MessageModule,
-    AbilityModule,
   ],
   providers: [
     JwtService,
@@ -56,7 +40,7 @@ import mikroOrmConfig from './mikro-orm.config';
   ],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
+  configure(/* consumer: MiddlewareConsumer */) {
     // consumer.apply(JwtMiddleware).forRoutes('*'); // Apply the jwt decrypting middleware to all routes
   }
 }
